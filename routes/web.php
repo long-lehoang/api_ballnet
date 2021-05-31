d97ca89e9c2fc755fe1d97b337fd01e8a44d6cfe
Long Le Hoang <long.lh@cloud9.local>
2021-05-31 13:48:34 +0700

    remove web route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-
-});
-Route::post('/testSignup','App\Http\Controllers\AuthController@signup');
